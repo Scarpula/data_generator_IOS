b864260df4fcce262c85e5522c8e486ace48b364
scarpula <dltjdeh7745@naver.com>
2026-01-29 14:28:43 +0900

    modified error

---
+++ HDMSDataGeneration/HDMSDataGeneration/HDMSDataGenerationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HDMSDataGenerationApp.swift
+//  HDMSDataGeneration
+//
+//  Created by 아이오티플러스주식회사 on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct HDMSDataGenerationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
